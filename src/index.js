11805620dbc06e5266147580c6409c85f58781b9
ViktorSoshenko <viktors103@gmail.com>
2023-04-16 11:25:27 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,48 +1,75 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetch';
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
+let inputSerch = '';
 
-input.addEventListener('input', event => {
-  event.currentTarget.value;
-  fetchCountries(event.currentTarget.value)
-    .then(users => renderUserList(users))
-    .catch(error => console.log(error));
-  console.log(event.currentTarget.value);
-});
-// fetch('https://restcountries.com/v3.1/all')
-//   .then(r => r.json())
-//   .then(console.log);
-//restcountries.com/v3.1/name/{name}
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const fields = 'fields=name,capital,population,flags,languages';
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}${input.value}?${fields}`)
-    .then(response => response.json())
-    .catch(error => console.error());
+input.addEventListener('input', debounce(renderUserList, DEBOUNCE_DELAY));
+
+function renderUserList(event) {
+  event.preventDefault();
+  inputSerch = input.value.trim();
+  if (inputSerch === '') {
+    clearAll();
+    return;
+  } else
+    fetchCountries(inputSerch)
+      .then(users => {
+        if (users.length > 10) {
+          moreTenElements();
+        } else if (users.length > 1 && users.length < 10) {
+          listCoutrys(users);
+        } else {
+          cartCountry(users);
+        }
+      })
+      .catch(error => {
+        clearAll();
+        if (error.message === '404') {
+          Notiflix.Notify.failure('Oops, there is no country with that name.');
+          console.log(error);
+        }
+      });
+}
+function moreTenElements() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+  return clearAll();
 }
 
-// function coutntry(name) {
-//   return fetch(
-//     `https://restcountries.com/v3.1/name/{${event.currentTarget.value}}`
-//   );
-// }
-
-function renderUserList(users) {
+function cartCountry(users) {
   const markup = users
     .map(user => {
-      return `
-          <li>
+      return `<li>
             <p><b>name</b>: ${user.name.official}</p>
             <p><b>capital </b>: ${user.capital}</p>
             <p><b>population </b>: ${user.population}</p>
+            <p><b>languages </b>: ${Object.values(user.languages).join(',')}</p>
              <p><IMG SRC="${user.flags.svg}" height = 40px></p>
-             <p><b>languages </b>: ${user.languages}</p>
-          </li>
+             </li>
       `;
     })
     .join('');
   list.innerHTML = markup;
-  console.log(users.languages);
+}
+
+function listCoutrys(users) {
+  const markup = users
+    .map(
+      user =>
+        `<li class=list-flex>
+        <p><IMG SRC="${user.flags.svg}" height = 40px></p>
+            <p>${user.name.official}</p>
+            </li>`
+    )
+    .join('');
+  list.innerHTML = markup;
+}
+
+function clearAll() {
+  list.innerHTML = '';
 }
